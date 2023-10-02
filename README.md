e1589571b3f9ffe24e5fd795f1470a8b4800157a
Shreyansh Keshav <shreykeshav33@gmail.com>
2023-10-02 15:25:38 +0530

    prototype1

---
+++ README.md
@@ -0,0 +1,18 @@
+Wireframe-
+
+
+-Header
+Logo
+Nav Items
+-Body
+Search
+RestaurantContainer
+ RestaurantCard
+ Img
+ Name of res, Star Rating, Cuisines
+-Footer
+Copyright
+Links
+Address
+Contact
+*/
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://image.similarpng.com/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
